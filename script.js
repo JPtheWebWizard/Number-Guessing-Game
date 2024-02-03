7fb0147d1464afdd999861017fd4d899882b0569
JPtheWebWizard <webdevprojectsbyjp@gmail.com>
2024-02-03 02:59:37 -0500

    first upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let running = true;
+const minNum = 1;
+const maxNum = 100;
+let guess;
+let attempts = 0;
+
+const answer;
+
+while (true) {
+    
+    
+
+
+
+
+    running = false;
+}
+
+function randomNumGen(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
